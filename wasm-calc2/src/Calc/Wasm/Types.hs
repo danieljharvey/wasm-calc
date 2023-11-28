09a1dc3f3f081651ce543ad73f17bd192f407631
Daniel Harvey <danieljamesharvey@gmail.com>
2023-11-28 18:00:49 +0000

    Convert `llvm-calc2` to `wasm-calc2` (#3)
    * Copy pasta llvm-calc2
    
    * Remove unused things

---
+++ wasm-calc2/src/Calc/Wasm/Types.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE DerivingStrategies #-}
+
+module Calc.Wasm.Types (Type (..), Module (..), Function (..)) where
+
+import Calc.Types.Expr
+
+data Type ann = I32
+  deriving stock (Eq, Ord, Show)
+
+newtype Module ann = Module
+  { -- | the functions themselves, their index comes from the list placement
+    modFunctions :: [Function ann]
+  }
+  deriving stock (Eq, Ord, Show)
+
+data Function ann = Function
+  { fnName :: String,
+    fnExpr :: Expr ann,
+    fnPublic :: Bool,
+    fnArgs :: [Type ann],
+    fnReturnType :: Type ann
+  }
+  deriving stock (Eq, Ord, Show)
