62f427bb38b4e32036d37275ca3fe8ba0c38b4e9
Daniel Harvey <danieljamesharvey@gmail.com>
2024-01-02 14:29:44 +0000

    Count uses

--- wasm-calc6/src/Calc/Linearity.hs
+++ wasm-calc6/src/Calc/Linearity.hs
@@ -1,24 +1,45 @@
-{-# LANGUAGE FlexibleContexts #-}
-module Calc.Linearity (getUses) where
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts   #-}
+{-# LANGUAGE NamedFieldPuns     #-}
+module Calc.Linearity (getUses, Linearity(..)) where
 
 import           Calc.ExprUtils
 import           Calc.Types.Expr
+import           Calc.Types.Function
 import           Calc.Types.Identifier
+import           Calc.Types.Type
 import           Control.Monad.State
 import qualified Data.Map              as M
-import           Data.Monoid
 import           GHC.Natural
 
-newtype LinearState =
-  LinearState { lsUses :: M.Map Identifier (Sum Natural) }
+-- | Primitive values can be used as much as you like, don't care
+data Linearity =
+    Primitive | Boxed Natural
+  deriving stock (Eq,Ord,Show)
 
-getUses :: Expr ann -> M.Map Identifier Natural
-getUses expr = fmap getSum $ lsUses $ execState
-    (decorateWithUses expr) (LinearState {lsUses = mempty})
+instance Semigroup Linearity where
+  (Boxed a) <> (Boxed b) = Boxed (a + b)
+  _ <> _                 = Primitive
+
+instance Monoid Linearity where
+  mempty = Boxed 0
+
+newtype LinearState =
+  LinearState { lsUses :: M.Map Identifier Linearity }
+
+getUses :: Function ann -> M.Map Identifier Linearity
+getUses (Function { fnBody,fnArgs}) = lsUses $ execState
+    (decorateWithUses fnBody) (LinearState {lsUses = initialUses})
+  where
+    initialUses =
+      foldMap (\(ArgumentName arg,ty) ->
+        M.singleton (Identifier arg) $ case ty of
+          TPrim {} -> Primitive
+          _        -> Boxed 0) fnArgs
 
 recordUse :: (MonadState LinearState m) => Identifier -> m ()
 recordUse ident =
-  modify (\ls -> ls { lsUses = M.insertWith mappend ident (Sum 1) (lsUses ls) })
+  modify (\ls -> ls { lsUses = M.insertWith mappend ident (Boxed 1) (lsUses ls) })
 
 decorateWithUses :: (MonadState LinearState m) => Expr ann -> m (Expr ann)
 decorateWithUses (EVar ann ident) = do
--- wasm-calc6/test/Test/Linearity/LinearitySpec.hs
+++ wasm-calc6/test/Test/Linearity/LinearitySpec.hs
@@ -16,21 +16,23 @@ spec = do
       let strings =
             [
               ( "function sum (a: Integer, b: Integer) { a + b }",
-                M.fromList [("a",1),("b",1)]
+                M.fromList [("a",Primitive),("b",Primitive)]
               ),
               ( "function pair<a,b>(a: a, b: b) { (a,b) }",
-                M.fromList [("a",1),("b",1)]
+                M.fromList [("a",Boxed 1),("b",Boxed 1)]
 
               ),
                ( "function dontUseA<a,b>(a: a, b: b) { b }",
-                M.fromList [("a",0),("b",1)]
+                M.fromList [("a",Boxed 0),("b",Boxed 1)]
+              ),
+               ( "function dup<a>(a: a) { (a,a)}",
+                M.fromList [("a",Boxed 2)]
               )
-
             ]
       traverse_
         ( \(str, uses) -> it (T.unpack str) $ do
             case parseFunctionAndFormatError str of
-              Right parsedFn -> getUses (fnBody parsedFn) `shouldBe` uses
+              Right parsedFn -> getUses parsedFn `shouldBe` uses
               Left e         -> error (T.unpack e)
         )
         strings
