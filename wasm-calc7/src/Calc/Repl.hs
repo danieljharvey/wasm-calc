1b125e290a29a9c8c4c8b0eedb6df16ef49ca432
Daniel Harvey <danieljamesharvey@gmail.com>
2024-01-28 10:04:56 +0000

    Need to export main in repl

--- wasm-calc7/src/Calc/Repl.hs
+++ wasm-calc7/src/Calc/Repl.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings     #-}
 
 {-# OPTIONS -Wno-orphans #-}
 
@@ -11,25 +11,23 @@ module Calc.Repl
   )
 where
 
-import Calc.Linearity
-  ( linearityErrorDiagnostic,
-    validateModule,
-  )
-import Calc.Parser
-import Calc.Parser.Types
-import Calc.Typecheck
-import Calc.Wasm.FromExpr
-import Calc.Wasm.Run
-import Calc.Wasm.ToWasm
-import Calc.Wasm.Types
-import Control.Monad.IO.Class
-import Data.Text (Text)
-import qualified Data.Text as T
-import Data.Void
-import qualified Error.Diagnose as Diag
-import Error.Diagnose.Compat.Megaparsec
-import qualified Language.Wasm.Interpreter as Wasm
-import System.Console.Haskeline
+import           Calc.Linearity                   (linearityErrorDiagnostic,
+                                                   validateModule)
+import           Calc.Parser
+import           Calc.Parser.Types
+import           Calc.Typecheck
+import           Calc.Wasm.FromExpr
+import           Calc.Wasm.Run
+import           Calc.Wasm.ToWasm
+import           Calc.Wasm.Types
+import           Control.Monad.IO.Class
+import           Data.Text                        (Text)
+import qualified Data.Text                        as T
+import           Data.Void
+import qualified Error.Diagnose                   as Diag
+import           Error.Diagnose.Compat.Megaparsec
+import qualified Language.Wasm.Interpreter        as Wasm
+import           System.Console.Haskeline
 
 instance HasHints Void msg where
   hints _ = mempty
@@ -76,7 +74,7 @@ wrapInputInFunction :: Text -> Text
 wrapInputInFunction input =
   if T.isInfixOf "function" input
     then input
-    else "function main() { " <> input <> " } "
+    else "export function main() { " <> input <> " } "
 
 runWasmModule :: WasmModule -> IO String
 runWasmModule mod' =
