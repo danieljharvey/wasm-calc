f489539aed1db365a61ef192e5d51c63ed12b807
Daniel Harvey <danieljamesharvey@gmail.com>
2024-07-05 23:17:59 +0100

    Wow

--- flake.nix
+++ flake.nix
@@ -7,87 +7,114 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-        # current compiler version, ideally, we'll put everything here
-        # eventually
+          # current compiler version, ideally, we'll put everything here
+          # eventually
 
-        compilerVersion = "ghc965";
+          compilerVersion = "ghc965";
 
-        # fix things
-        haskell = pkgs.haskell // {
-          packages = pkgs.haskell.packages // {
-            "${compilerVersion}" =
-              pkgs.haskell.packages."${compilerVersion}".override {
-                overrides = self: super: {
-                  # try and remove cycle
-                  cabal-fmt = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.overrideCabal super.cabal-fmt (drv: {
-                    enableSeparateBinOutput = false;
-                  }));
-                  # try and remove cycle
-                  ormolu = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.overrideCabal super.ormolu (drv: {
-                    enableSeparateBinOutput = false;
-                  }));
+          # fix things
+          haskell = pkgs.haskell // {
+            packages = pkgs.haskell.packages // {
+              "${compilerVersion}" =
+                pkgs.haskell.packages."${compilerVersion}".override {
+                  overrides = self: super: {
+                    # try and remove cycle
+                    cabal-fmt = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.overrideCabal super.cabal-fmt (drv: {
+                      enableSeparateBinOutput = false;
+                    }));
+                    # try and remove cycle
+                    ormolu = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.overrideCabal super.ormolu (drv: {
+                      enableSeparateBinOutput = false;
+                    }));
 
-                  diagnose = (pkgs.haskell.lib.doJailbreak
-                  (pkgs.haskell.lib.appendConfigureFlags (pkgs.haskell.lib.markUnbroken super.diagnose) ["-f +megaparsec-compat" "-f parsec-compat"])).overrideAttrs
-                    (oldAttrs: rec {
-        buildInputs = [super.megaparsec];
-      });
+                    diagnose = (pkgs.haskell.lib.doJailbreak
+                      (pkgs.haskell.lib.appendConfigureFlags (pkgs.haskell.lib.markUnbroken super.diagnose) [ "-f +megaparsec-compat" "-f parsec-compat" ])).overrideAttrs
+                      (oldAttrs: rec {
+                        buildInputs = [ super.megaparsec ];
+                      });
 
 
-                  # allow slippery version bounds and don't run tests
-                  wasm = pkgs.haskell.lib.markUnbroken (pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.doJailbreak super.wasm));
+                    # allow slippery version bounds and don't run tests
+                    wasm = pkgs.haskell.lib.markUnbroken (pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.doJailbreak super.wasm));
+                  };
+
                 };
-
-              };
+            };
           };
-        };
 
-        haskellPackages = haskell.packages.${compilerVersion};
+          haskellPackages = haskell.packages.${compilerVersion};
 
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+          jailbreakUnbreak = pkg:
+            pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
 
-        packageName = "wasm-calc";
-      in
-      {
-        packages.wasm-calc9 =
-          pkgs.haskell.lib.overrideCabal (haskellPackages.callCabal2nix "wasm-calc9" ./wasm-calc9 {
-            # Dependency overrides go here
+          packageName = "wasm-calc";
 
-             })               (old:
-             {
-                # copy the client into the package
-               testToolDepends = [pkgs.nodejs];
-              });
+          makeCalc = name: dir:
+            pkgs.haskell.lib.overrideCabal
+              (haskellPackages.callCabal2nix name dir {
+                # Dependency overrides go here
+              })
+              (old:
+                {
+                  # copy the client into the package
+                  testToolDepends = [ pkgs.nodejs ];
+                });
 
-        defaultPackage = self.packages.${system}.wasm-calc9;
+          wasm-calc = makeCalc "wasm-calc" ./wasm-calc;
+          wasm-calc2 = makeCalc "wasm-calc2" ./wasm-calc2;
+          wasm-calc3 = makeCalc "wasm-calc3" ./wasm-calc3;
+          wasm-calc4 = makeCalc "wasm-calc4" ./wasm-calc4;
+          wasm-calc5 = makeCalc "wasm-calc5" ./wasm-calc5;
+          wasm-calc6 = makeCalc "wasm-calc6" ./wasm-calc6;
+          wasm-calc7 = makeCalc "wasm-calc7" ./wasm-calc7;
+          wasm-calc8 = makeCalc "wasm-calc8" ./wasm-calc8;
+          wasm-calc9 = makeCalc "wasm-calc9" ./wasm-calc9;
+          wasm-calc10 = makeCalc "wasm-calc10" ./wasm-calc10;
 
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with haskellPackages; [ pkgs.git pkgs.nodejs ];
+        in
+        {
+          packages =
+            {
+              inherit wasm-calc wasm-calc2 wasm-calc3
+                wasm-calc4 wasm-calc5 wasm-calc6
+                wasm-calc7 wasm-calc8 wasm-calc9 wasm-calc10;
+            };
 
-          buildInputs = with haskellPackages; [
-            ghc
-            hlint
-            ormolu
-            #haskell-language-server
-            pkgs.ghciwatch
-            cabal-fmt
-            cabal-install
-            # test tooling
-            pkgs.git
-            pkgs.nodejs
-            pkgs.watchexec
-            pkgs.nodePackages_latest.serve
-            #pkgs.wabt
-            #pkgs.emscripten
-            #pkgs.wasmtime
-          ];
+          # sneaky way to 'build all'
+          checks = {
+            inherit wasm-calc wasm-calc2 wasm-calc3
+              wasm-calc4 wasm-calc5 wasm-calc6
+              wasm-calc7 wasm-calc8 wasm-calc9
+              wasm-calc10;
+          };
 
-          inputsFrom = builtins.attrValues self.packages.${system};
-        };
-      });
+          defaultPackage = self.packages.${system}.wasm-calc9;
+
+          devShell = pkgs.mkShell {
+
+            buildInputs = with haskellPackages; [
+              ghc
+              hlint
+              ormolu
+              #haskell-language-server
+              pkgs.ghciwatch
+              cabal-fmt
+              cabal-install
+
+              # test tooling
+              pkgs.nixpkgs-fmt
+              pkgs.git
+              pkgs.nodejs
+              pkgs.watchexec
+              pkgs.nodePackages_latest.serve
+            ];
+
+            inputsFrom = builtins.attrValues self.packages.${system};
+          };
+        });
 }
